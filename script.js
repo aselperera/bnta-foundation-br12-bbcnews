89d782856469a3c7456b1f643383324e6aad6877
aselperera <aselperera@hotmail.com>
2021-02-03 21:55:58 +0000

    AP: Adding JavaScript
    Adding JavaScript for opening to header

---
+++ script.js
@@ -0,0 +1,33 @@
+/*
+$(document).ready(function(){
+    var blink = document.getElementById.apply("blink-text");
+    setInterval(function(){
+        blink.style.display = (blink.style.display == 'none' ? '' : 'none');
+    }, 1000);
+
+})(jQuery);
+*/
+
+// Temporarily disabled 
+
+/*
+window.addEventListener("load", function(){
+    var blink = document.getElementById("blink-text");
+    setInterval(function(){
+        blink.style.display = (blink.style.display == 'none' ? '' : 'none');
+    }, 500);
+
+}, false);
+*/
+
+// Open top dropdown menu code
+
+function dropdownToggle() {
+    var dropdownTop = document.getElementById("dropdown-top");
+    if (dropdownTop.style.display == "none") {
+        dropdownTop.style.display = "block";
+    }
+    else {
+        dropdownTop.style.display = "none";
+    }
+}
