7494eaade48b476dc1f59a0a1def6746d5cf362a
aselperera <aselperera@hotmail.com>
2021-02-04 15:24:41 +0000

    AP: Additional JavaScript test

--- script.js
+++ script.js
@@ -34,7 +34,81 @@ function dropdownToggle() {
 
 // Datetime
 
-var today = new Date();
-var date = today.getFullYear() + "-" + today.getMonth+1 + today.getDate();
-var time = today.getHours() + ":" + today.getMinutes()
-var dateTime = date + " " + time;
+function setDateTime() {
+
+    let today = new Date();
+    let date = today.getDay() + "-" + today.getMonth() + "-" + today.getFullYear(); 
+    let time = today.getHours() + ":" + today.getMinutes();
+    let dateTime = date + " " + time;
+
+    let dateTimeText = document.getElementById("datetime-text");
+    dateTimeText.innerHTML = dateTime;
+
+    setInterval(() => {
+        
+        let today = new Date();
+        let date = today.getDay() + "-" + today.getMonth() + "-" + today.getFullYear(); 
+        let time = today.getHours() + ":" + today.getMinutes();
+        let dateTime = date + " " + time;
+
+        let dateTimeText = document.getElementById("datetime-text");
+        dateTimeText.innerHTML = dateTime;
+
+    }, 10000);
+
+}
+
+// Scroll to top button
+
+window.onscroll = function() {scrollButtonAppear()};
+
+function scrollButtonAppear() {
+    document.getElementById("scroll-top-button").style.display = "block";
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, Opera
+}
+
+// Retrieve weather
+
+/*
+function getWeather() {
+    var xhttp = new XMLHttpRequest();
+    document.getElementById("weather-button").innerHTML = this.responseText;
+
+    const api_host = "https://www.metaweather.com/44418/";
+    xhttp.open("GET", "https://cors-anywhere.herokuapp.com/" + api_host, true);
+    xhttp.send();
+    
+}
+*/
+
+function weatherBalloon( cityID ) {
+    var key = '02fda04ac7b274cbed337f6b2945734b';
+    fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
+    .then(function(resp) { return resp.json() }) // Convert data to json
+    .then(function(data) {
+      console.log(data);
+    })
+    .catch(function() {
+      // catch any errors
+    });
+}
+  
+window.onload = function() {
+    weatherBalloon( 6167865 );
+}
+
+window.addEventListener("load", () => {
+    let lat;
+    let long;
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+        })
+    }
+});
